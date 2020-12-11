a6186cc5324ded69af88f5e97c1734b48d343dc1
arumxps <74328204+arumxps@users.noreply.github.com>
2020-12-11 13:44:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// START HAMBURGER BUTTON
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+function close() {
+    mainMenu.style.top = '-100%';
+}
+
+// END HAMBURGER BUTTON
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
